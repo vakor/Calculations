c873aa33d3c08ecfb71e5e22537523db66ce11a2
Вася <vakor-94@mail.ru>
2015-02-09 22:33:20 +0200

    http://www.itmathrepetitor.ru/prog/zadachi-na-vychisleniya/ 10

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 
 public class Arithmetic {
 
@@ -17,4 +19,39 @@ public class Arithmetic {
 	public static float vuraz4 (float x) {
 		return  (float) ( Math.exp(x-2) + Math.abs(Math.sin(x) ) - Math.pow(x, 10) * Math.cos(1/x) );	
 	}
+	 static float vuraz5 (float x, float a, float b) {
+		return  (float) ( Math.pow( x*x + b , 1/5) - b*b * Math.pow( Math.sin(x + a), 3) / x );	
+	}
+	static void userSumMultiple(){
+		Scanner in = new Scanner(System.in);
+		double a = in.nextDouble();
+		double b = in.nextDouble();
+		System.out.println(a+b);
+		System.out.println(a*b);
+		in.close();
+	}
+	static void userAvg(){
+		Scanner in = new Scanner(System.in);
+		double a = in.nextDouble();
+		double b = in.nextDouble();
+		double c = in.nextDouble();
+		System.out.println((a+b+c)/3);
+		System.out.println(2 * (a+c)- 3 * b);
+		in.close();
+	}
+	static void userSquare(){
+		Scanner in = new Scanner(System.in);
+		double a = in.nextDouble();
+		System.out.println("Периметр " + 4*a);
+		System.out.println("Площа " + a*a);
+		in.close();
+	}
+	static void userKm_mToM_s(){
+		Scanner in = new Scanner(System.in);
+		double l = in.nextDouble();
+		double t = in.nextDouble();
+		System.out.println( ((l*1000)/ t)/60);
+		in.close();
+	}
 }
+
--- src/mainCalculation.java
+++ src/mainCalculation.java
@@ -4,7 +4,7 @@ public class mainCalculation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Console cons = new Console();
+		//Console cons = new Console();
 		//cons.MyFirstText();
 		//cons.GetDayMonth();
 		//cons.Strings0();
@@ -17,11 +17,16 @@ public class mainCalculation {
 		//System.out.println(Arithmetic.vuraz2(x));
 		//float x = -2.34f;
 		//System.out.println(Arithmetic.vuraz2(x));
-		float x = 3.6f;
-		System.out.println(Arithmetic.vuraz4(x));
-	
-	
-	
+		//float x = 3.6f;
+		//System.out.println(Arithmetic.vuraz4(x));
+		//float a = 0.1f;
+		//float b = 0.2f;
+		//float x = 1f;
+		//System.out.println(Arithmetic.vuraz5(x, a, b));
+		//Arithmetic.userSumMultiple();
+		//Arithmetic.userAvg();
+		//Arithmetic.userSquare();
+		Arithmetic.userKm_mToM_s();
 	}
 
 }
